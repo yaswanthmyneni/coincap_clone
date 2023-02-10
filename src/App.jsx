2088111b72612504fcb9f49bae07526c9529c742
yaswanthmyneni <yaswanthmyneni@gmail.com>
2023-02-10 10:56:13 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,7 @@ import Home from "./pages/home/Home";
 
 class App extends React.Component {
   render() {
-    return (
-      <section>
-        <Home />
-      </section>
-    );
+    return <Home />;
   }
 }
 
